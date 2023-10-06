6c4a3fc7b690245e66bf719d7cc0c93263745604
Alexander Luna <alexanderfluna@outlook.com>
2023-10-06 12:09:10 -0400

    Update suggestions

--- src/timeline/Suggestions.js
+++ src/timeline/Suggestions.js
@@ -1,9 +1,67 @@
+import { Avatar } from "@mui/material"
 import React from 'react'
 import "./Suggestions.css"
 
 function Suggestions() {
   return (
-    <div>Suggestions</div>
+    <div className="suggestions">
+      <div className="suggestions-title">Suggestions for you</div>
+      <div className="suggestions-usernames">
+
+        <div className="suggestion-username">
+          <div className="username-left">
+            <span className="avatar">
+            <Avatar>R</Avatar>
+            </span>
+            <div class="username-info">
+              <span class="username">Alexander</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+      
+        <div className="suggestion-username">
+          <div className="username-left">
+            <span className="avatar">
+            <Avatar>R</Avatar>
+            </span>
+            <div class="username-info">
+              <span class="username">Alexander</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+      
+        <div className="suggestion-username">
+          <div className="username-left">
+            <span className="avatar">
+            <Avatar>R</Avatar>
+            </span>
+            <div class="username-info">
+              <span class="username">Alexander</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+      
+        <div className="suggestion-username">
+          <div className="username-left">
+            <span className="avatar">
+            <Avatar>R</Avatar>
+            </span>
+            <div class="username-info">
+              <span class="username">Alexander</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
