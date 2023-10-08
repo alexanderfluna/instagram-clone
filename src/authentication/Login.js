093403970cf30244325b14cfcf45cc60a1709584
Alexander Luna <alexanderfluna@outlook.com>
2023-10-08 12:34:30 -0400

    Modifications

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./Login.css"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase";
+import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password);
+  }
+  
   return (
     <div className="login">
       <img src="https://th.bing.com/th/id/R.58e1880fc14bf0c89a7967c2792bcece?rik=YXXhJ847THBahQ&pid=ImgRaw&r=0" alt="Instagram"></img>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+
+      <input 
+      onChange={e => setEmail(e.target.value)} 
+      type="email" 
+      placeholder="Email"
+      value = {email}
+      />
+
+      <input 
+      onChange={e => setPassword(e.target.value)} 
+      type="password" placeholder="Password" 
+      value = {password}
+      />
+      
       <button>Log in</button>
     </div>
   )
--- src/authentication/Signup.js
+++ src/authentication/Signup.js
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Signup.css"
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../firebase"
 
 function Signup() {
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSignUp = (event) => {
+    event.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((authUser) => {
+      signInWithEmailAndPassword(auth, email, password)
+      .then(updateProfile(auth.currentUser, {
+        displayName: username,
+      }))
+      .catch((err) => {
+        alert(err)
+      })
+    })
+  }
+
   return (
     <div className="signup">
       <img src="https://th.bing.com/th/id/R.58e1880fc14bf0c89a7967c2792bcece?rik=YXXhJ847THBahQ&pid=ImgRaw&r=0" alt="Instagram"></img>
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Sign in</button>
+
+      <input 
+      onChange={e => setEmail(e.target.value)} 
+      type="email" 
+      placeholder="Email"
+      value = {email}
+      />
+
+      <input 
+      onChange={e => setUsername(e.target.value)}
+      type="text"
+      placeholder="Username"
+      value = {username}
+      />
+
+
+      <input 
+      onChange={e => setPassword(e.target.value)} 
+      type="password" placeholder="Password" 
+      value = {password}
+      />
+
+      <button onClick={handleSignUp}>Sign in</button>
     </div>
   )
 }
